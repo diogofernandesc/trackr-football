b1f1a70d3594c67c4912941cb8a8c253d49baecc
Diogo Fernandes <diogofernandescon@gmail.com>
2019-02-05 17:42:17 +0000

    added parser for match details and advanced match details included H2H

--- ingest_engine/cons.py
+++ ingest_engine/cons.py
@@ -47,6 +47,7 @@ class Match:
     SEASON_END_DATE = 'season_end_date'
     SEASON_YEAR = 'season_year'
     MATCH_UTC_DATE = 'utc_date'
+    START_TIME_EPOCH = 'start_time_epoch'
     STATUS = 'status'
     MATCHDAY = 'matchday'
     FULL_TIME_HOME_SCORE = 'full_time_home_score'
@@ -62,9 +63,45 @@ class Match:
     AWAY_TEAM = 'away_team'
     REFEREES = 'referees'
     FILTERS = 'filters'
-    FASTEST_LIVE_SCORES_API_ID = 'fls_api_id'
+    FLS_MATCH_ID = 'fls_match_id'
     FLS_API_COMPETITION_ID = 'fls_competition_id'
     COMPETITION = 'competition'
+    EVENTS = 'events'
+    HOME_SCORE_PROBABILITY = 'home_score_probability'
+    HOME_CONCEDE_PROBABILITY = 'home_concede_probability'
+    AWAY_SCORE_PROBABILITY = 'away_score_probability'
+    AWAY_CONCEDE_PROBABILITY = 'away_concede_probability'
+    HOME_SCORE_PROBABILITY_OVER_1_5 = 'home_score_probability_over_1_5'  # THESE STATS ARE BASED ON THE LAST 5 GAMES
+    HOME_SCORE_PROBABILITY_OVER_2_5 = 'home_score_probability_over_2_5'
+    HOME_SCORE_PROBABILITY_OVER_3_5 = 'home_score_probability_over_3_5'
+    HOME_SCORE_PROBABILITY_UNDER_1_5 = 'home_score_probability_under_1_5'
+    HOME_SCORE_PROBABILITY_UNDER_2_5 = 'home_score_probability_under_2_5'
+    HOME_SCORE_PROBABILITY_UNDER_3_5 = 'home_score_probability_under_3_5'
+    AWAY_SCORE_PROBABILITY_OVER_1_5 = 'away_score_probability_over_1_5'
+    AWAY_SCORE_PROBABILITY_OVER_2_5 = 'away_score_probability_over_2_5'
+    AWAY_SCORE_PROBABILITY_OVER_3_5 = 'away_score_probability_over_3_5'
+    AWAY_SCORE_PROBABILITY_UNDER_1_5 = 'away_score_probability_under_1_5'
+    AWAY_SCORE_PROBABILITY_UNDER_2_5 = 'away_score_probability_under_2_5'
+    AWAY_SCORE_PROBABILITY_UNDER_3_5 = 'away_score_probability_under_3_5'
+    HOME_FORM = 'home_form'
+    AWAY_FORM = 'away_form'
+    HOME_TEAM_FLS_ID = 'home_team_fls_id'
+    AWAY_TEAM_FLS_ID = 'away_team_fls_id'
+    PREVIOUS_ENCOUNTERS = 'previous_encounters'
+    PENALTY_SHOOTOUT_SCORE = 'penalty_shootout_score'
+
+class MatchEvent:
+    OCCURED_AT = 'occured_at'
+    STATE = 'state'
+    MINUTES_PASSED = 'minutes_passed'
+    OWN_GOALS = 'own_goals'
+    ADDED_TIME = 'added_time'
+
+    HOME_GOALS = 'home_goals'
+    AWAY_GOALS = 'away_goals'
+    SCORER_FLS_ID = 'scorer_fls_id'
+    ASSIST_FLS_ID = 'assist_fls_id'
+    TYPE = 'type'
 
 
 class Team:
@@ -116,12 +153,18 @@ class Player:
     FIRST_NAME = 'first_name'
     LAST_NAME = 'last_name'
     DATE_OF_BIRTH = 'date_of_birth'
+    DATE_OF_BIRTH_EPOCH = 'date_of_birth_epoch'
     COUNTRY_OF_BIRTH = 'country_of_birth'
     NATIONALITY = 'nationality'
     POSITION = 'position'
     SHIRT_NUMBER = 'shirt_number'
     TEAM = 'team'
     NUMBER_OF_GOALS = 'number_of_goals'
+    WEIGHT = 'weight'
+    GENDER = 'gender'
+    HEIGHT = 'height'
+
+
 
 
 class Competition:
@@ -140,4 +183,34 @@ class Season:
     WINNER = 'winner'
     FOOTBALL_DATA_API_ID = 'football_data_api_id'
 
+
+class FLS_STATES:
+    FIXTURE = 0
+    FIRST_HALF = 1
+    HALF_TIME = 2
+    SECOND_HALF = 3
+    EXTRA_TIME = 4
+    EXTRA_TIME_FIRST_HALF = 5
+    EXTRA_TIME_INTERVAL = 6
+    EXTRA_TIME_SECOND_HALF = 7
+    PENALTIES = 8
+    FULL_TIME = 9
+    ABANDONED = 101
+    POSTPONED = 102
+
+
+FLS_STATES_MAPPER = {
+    0: "fixture",
+    1: "first_half",
+    2: "half_time",
+    3: "second_half",
+    4: "extra_time",
+    5: "extra_time_first_half",
+    6: "extra_time_interval",
+    7: "extra_time_second_half",
+    8: "penalties",
+    9: "full_time",
+    101: "abandoned",
+    102: "postponed"
+}
 
--- ingest_engine/fastest_live_scores_api.py
+++ ingest_engine/fastest_live_scores_api.py
@@ -2,7 +2,7 @@ from ratelimit import limits, sleep_and_retry
 import requests as re
 import os
 import json
-from ingest_engine.cons import Competition, Team, Match
+from ingest_engine.cons import Competition, Team, Match, MatchEvent, FLS_STATES_MAPPER as state_mapper
 
 HOUR = 3600
 
@@ -144,14 +144,119 @@ class FastestLiveScores(object):
         """
         endpoint = self.build_endpoint(endpoint_name=f"matches/{match_id}")
         result = self.perform_get(built_uri=endpoint)
-        total_result = []
+        additional_data_endpoint = self.build_endpoint(endpoint_name=f"matches/{match_id}/additional-data")
+        additional_data = self.perform_get(built_uri=additional_data_endpoint)
+        match_events = []
+        dict_result = {}
+
+        if additional_data:
+            if "additionalStats" in additional_data:
+                additional_stats = additional_data["additionalStats"]
+                dict_result[Match.HOME_SCORE_PROBABILITY] = additional_stats["home"]["probabilityToScore"]
+                dict_result[Match.HOME_CONCEDE_PROBABILITY] = additional_stats["home"]["probabilityToConcede"]
+                dict_result[Match.AWAY_SCORE_PROBABILITY] = additional_stats["away"]["probabilityToScore"]
+                dict_result[Match.AWAY_CONCEDE_PROBABILITY] = additional_stats["away"]["probabilityToConcede"]
+                dict_result[Match.HOME_FORM] = additional_stats["home"]["form"]
+                dict_result[Match.AWAY_FORM] = additional_stats["away"]["form"]
+
+                if "scoreBracketRecord" in additional_stats["home"]:
+                    dict_result[Match.HOME_SCORE_PROBABILITY_OVER_1_5] = \
+                        additional_stats["home"]["scoreBracketRecord"]["1.5"]["over"]
+
+                    dict_result[Match.HOME_SCORE_PROBABILITY_OVER_2_5] = \
+                        additional_stats["home"]["scoreBracketRecord"]["2.5"]["over"]
+
+                    dict_result[Match.HOME_SCORE_PROBABILITY_OVER_3_5] = \
+                        additional_stats["home"]["scoreBracketRecord"]["3.5"]["over"]
+
+                    dict_result[Match.HOME_SCORE_PROBABILITY_UNDER_1_5] = \
+                        additional_stats["home"]["scoreBracketRecord"]["1.5"]["under"]
+
+                    dict_result[Match.HOME_SCORE_PROBABILITY_UNDER_2_5] = \
+                        additional_stats["home"]["scoreBracketRecord"]["2.5"]["under"]
+
+                    dict_result[Match.HOME_SCORE_PROBABILITY_UNDER_3_5] = \
+                        additional_stats["home"]["scoreBracketRecord"]["3.5"]["under"]
+
+                if "scoreBracketRecord" in additional_stats["away"]:
+                    dict_result[Match.AWAY_SCORE_PROBABILITY_OVER_1_5] = \
+                        additional_stats["away"]["scoreBracketRecord"]["1.5"]["over"]
+
+                    dict_result[Match.AWAY_SCORE_PROBABILITY_OVER_2_5] = \
+                        additional_stats["away"]["scoreBracketRecord"]["2.5"]["over"]
+
+                    dict_result[Match.AWAY_SCORE_PROBABILITY_OVER_1_5] = \
+                        additional_stats["away"]["scoreBracketRecord"]["3.5"]["over"]
+
+                    dict_result[Match.AWAY_SCORE_PROBABILITY_UNDER_1_5] = \
+                        additional_stats["away"]["scoreBracketRecord"]["1.5"]["under"]
+
+                    dict_result[Match.AWAY_SCORE_PROBABILITY_UNDER_2_5] = \
+                        additional_stats["away"]["scoreBracketRecord"]["2.5"]["under"]
+
+                    dict_result[Match.AWAY_SCORE_PROBABILITY_UNDER_3_5] = \
+                        additional_stats["away"]["scoreBracketRecord"]["3.5"]["under"]
+
+            if "headToHead" in additional_data:
+                head_to_head = additional_data["headToHead"]
+                previous_encounters = []
+                for encounter in head_to_head:
+                    previous_encounters.append({
+                        Match.HOME_TEAM_FLS_ID: encounter["homeTeam"]["dbid"],
+                        Match.AWAY_TEAM_FLS_ID: encounter["awayTeam"]["dbid"],
+                        Match.HOME_TEAM: encounter["homeTeam"]["name"],
+                        Match.AWAY_TEAM: encounter["awayTeam"]["name"],
+                        Match.START_TIME_EPOCH: encounter["start"],
+                        Match.FULL_TIME_HOME_SCORE: encounter["homeGoals"],
+                        Match.FULL_TIME_AWAY_SCORE: encounter["awayGoals"],
+                        Match.WINNER: encounter["outcome"]["winner"],
+                        Match.PENALTY_SHOOTOUT_SCORE: encounter["penaltyShootoutScore"]
+                    })
+
+                if previous_encounters:
+                    dict_result[Match.PREVIOUS_ENCOUNTERS] = previous_encounters
 
         if result:
-            result = result["matchevents"]
-            # for phase in result:
+            if "matchevents" in result:
+                result = result["matchevents"]
+
+                for event in result:
+                    match_time = event['matchTime']
+                    data = {
+                        MatchEvent.STATE: state_mapper.get(match_time['state']),
+                        MatchEvent.MINUTES_PASSED: match_time['minutes'],
+                        MatchEvent.ADDED_TIME: match_time['extra'],
+                        Match.HOME_TEAM_FLS_ID: event['homeTeam']['dbid'],
+                        Match.AWAY_TEAM_FLS_ID: event['awayTeam']['dbid'],
+                        Match.FLS_MATCH_ID: event['matchId'], MatchEvent.OCCURED_AT: event['happenedAt'],
+                        MatchEvent.TYPE: event['type']
+                    }
+
+                    if Match.HOME_TEAM_FLS_ID not in dict_result:
+                        dict_result[Match.HOME_TEAM_FLS_ID] = event['homeTeam']['dbid']
+
+                    if Match.AWAY_TEAM_FLS_ID not in dict_result:
+                        dict_result[Match.AWAY_TEAM_FLS_ID] = event['awayTeam']['dbid']
+
+
+                    if 'scoringPlayer' in event:
+                        if event['scoringPlayer']:
+                            data[MatchEvent.SCORER_FLS_ID] = event['scoringPlayer']['dbid']
+
+                    if 'assistingPlayer' in event:
+                        if event['assistingPlayer']:
+                            data[MatchEvent.ASSIST_FLS_ID] = event['assistingPlayer']['dbid']
+
+                    match_events.append(data)
+
+        if match_events:
+            dict_result[Match.EVENTS] = match_events
+
+        return dict_result
+
 
 if __name__ == "__main__":
     fls = FastestLiveScores(api_key=os.getenv('FASTEST_LIVE_SCORES_API_KEY'))
-    print(fls.request_matches(competition_id=60))
+    print(fls.request_match_details(match_id=321042))
 
 
