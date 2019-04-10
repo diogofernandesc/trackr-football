1de03509ca636019cf4c3fa4fe8c4bd55f8a2547
Diogo Fernandes <diogofernandescon@gmail.com>
2019-04-10 10:11:17 +0200

    baseline code for querying competition records

--- db_engine/db_interface.py
+++ db_engine/db_interface.py
@@ -3,7 +3,7 @@ from ingest_engine.cons import Player as PLAYER
 import logging
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from db_engine.db_driver import Player
+from db_engine.db_driver import Player, Competition
 import os
 
 # logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
@@ -22,9 +22,10 @@ class QueryEngine(object):
         print(os.getenv('POSTGRES_CONNECTION_STR'))
         self.db = SQLAlchemy(self.app)
 
-    def get_player(self, fls_api_id=None, fd_api_id=None, name=None):
+    def get_player(self, id=None, fls_api_id=None, fd_api_id=None, name=None):
         """
         Query DB for player record
+        :param id: DB ID of player
         :param fls_api_id: fastestlivescores API id for player
         :param fd_api_id: football-data.org API id for player
         :param name: name of player used for SQL LIKE search
@@ -44,6 +45,34 @@ class QueryEngine(object):
         player_query = player_query.all()
 
         return player_query
+
+    def get_competition(self, name=None, code=None, location=None, fd_api_id=None, fls_api_id=None):
+        """
+        Query DB for competition record
+        :param name: The name of the competition, LIKE match performed
+        :param code: The code of the competition
+        :param location: Country/Location of the competition
+        :param fd_api_id: Football data API id for competition
+        :param fls_api_id: FastestLiveScores API id for competition
+        :return:  matched (if any) competition records
+        """
+        comp_query = Competition.query
+        if name:
+            comp_query = comp_query.filter(Competition.name.like(f"%{name}%"))
+
+        if code:
+            comp_query = comp_query.filter_by(code=code)
+
+        if location:
+            comp_query = comp_query.filter_by(location=location)
+
+        if fd_api_id:
+            comp_query = comp_query.filter_by(football_data_api_id=fd_api_id)
+
+        if fls_api_id:
+            comp_query = comp_query.filter_by(fls_api_id=fls_api_id)
+
+        
 
 
 if __name__ == "__main__":
