b2382d604a7e21fc7e9ceec6511e4e77383053ad
Diogo Fernandes <diogofernandescon@gmail.com>
2019-05-03 16:06:52 +0100

    added info for filtering

--- db_engine/db_filters.py
+++ db_engine/db_filters.py
@@ -2,6 +2,12 @@ from collections import namedtuple
 from ingest_engine.cons import Team as TEAM
 
 
+"""
+To add a new table filter:
+ - Add a list of filters, preferably a list of string constants representing the column names that can be filtered on
+ - Create a named tuple with default none fields, passing your newly defined list of filterable columns
+ """
+
 team_filter_fields = [TEAM.ID,
                       TEAM.FANTASY_ID,
                       TEAM.FOOTBALL_DATA_ID,
