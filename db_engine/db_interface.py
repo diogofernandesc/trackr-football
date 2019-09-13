98c793b58e12ef685e88294b4a4c9458db4febd1
Diogo <diogofernandescon@gmail.com>
2019-09-13 13:21:13 +0100

    Api service pre deploy changes (#37)
    * Added outer join for players
    
    * Added outer join for players
    
    * Added check for if match exists
    
    * Dont check for existence of mathc object, check on insert (upsert)
    
    * Removed unecessary test

--- db_engine/db_interface.py
+++ db_engine/db_interface.py
@@ -54,11 +54,12 @@ def filter_parse(query_str, table, column):
 
 def to_json(result_map, aggregator_name, limit=10):
     list_dict_result = []
-    for k, v in result_map.items():
-        dict_result = k.__dict__
-        dict_result.pop(IGNORE.INSTANCE_STATE, None)
-        dict_result[aggregator_name] = clean_output(v, as_list=True)
-        list_dict_result.append(dict_result)
+    if result_map:
+        for k, v in result_map.items():
+            dict_result = k.__dict__
+            dict_result.pop(IGNORE.INSTANCE_STATE, None)
+            dict_result[aggregator_name] = clean_output(v, as_list=True)
+            list_dict_result.append(dict_result)
 
     if len(list_dict_result) == 1:
         return list_dict_result[0]
@@ -205,7 +206,7 @@ class DBInterface(object):
         db_filters = []
         player_query = self.db.session\
             .query(Player, FantasyWeekStats)\
-            .join(FantasyWeekStats, Player.id == FantasyWeekStats.player_id)
+            .outerjoin(FantasyWeekStats, Player.id == FantasyWeekStats.player_id)
 
         active_filters = [(f, v) for f, v in filters._asdict().items() if v]
 
@@ -233,9 +234,9 @@ class DBInterface(object):
         player_week_stat_map = {}
         for player, week_stat in query_result:
             if player not in player_week_stat_map:
-                player_week_stat_map[player] = [week_stat]
+                player_week_stat_map[player] = []
 
-            else:
+            if week_stat:
                 player_week_stat_map[player].append(week_stat)
 
         result = to_json(player_week_stat_map, aggregator_name=PLAYER.WEEK_STATS, limit=limit)
@@ -438,20 +439,22 @@ class DBInterface(object):
             record = [record]
 
         for match in record:
-            match.pop(MATCH.GOALS_SCORED, None)
-            match.pop(MATCH.EVENTS, None)
-            match.pop(MATCH.ASSISTS, None)
-            match.pop(MATCH.OWN_GOALS, None)
-            match.pop(MATCH.BPS, None)
-            match.pop(MATCH.BONUS, None)
-            match.pop(MATCH.BPS, None)
-            match.pop(MATCH.PENALTIES_SAVED, None)
-            match.pop(MATCH.PENALTIES_MISSED, None)
-            match.pop(MATCH.YELLOW_CARDS, None)
-            match.pop(MATCH.RED_CARDS, None)
-            match.pop(MATCH.SAVES, None)
-            match.pop(MATCH.PREVIOUS_ENCOUNTERS, None)
-            self.db.session.add(Match(**match))
+            match_query = self.db.session.query(Match).filter(Match.match_fd_id == match[MATCH.FOOTBALL_DATA_ID])
+            if not match_query.count():
+                match.pop(MATCH.GOALS_SCORED, None)
+                match.pop(MATCH.EVENTS, None)
+                match.pop(MATCH.ASSISTS, None)
+                match.pop(MATCH.OWN_GOALS, None)
+                match.pop(MATCH.BPS, None)
+                match.pop(MATCH.BONUS, None)
+                match.pop(MATCH.BPS, None)
+                match.pop(MATCH.PENALTIES_SAVED, None)
+                match.pop(MATCH.PENALTIES_MISSED, None)
+                match.pop(MATCH.YELLOW_CARDS, None)
+                match.pop(MATCH.RED_CARDS, None)
+                match.pop(MATCH.SAVES, None)
+                match.pop(MATCH.PREVIOUS_ENCOUNTERS, None)
+                self.db.session.add(Match(**match))
 
         self.db.session.commit()
 
